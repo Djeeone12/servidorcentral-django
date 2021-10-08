8a99d5f76f4b2b97b251ca75520badae68547d6a
Alex Gonzalez <durandalex1259@gmail.com>
2021-10-07 21:53:36 -0500

    Poke HTTPS

--- centralserver/ejercicio/views.py
+++ centralserver/ejercicio/views.py
@@ -10,3 +10,15 @@ def index(request):
 
 def list_view(request):
     return render(request, 'listas1.html')
+
+def poke_get(request):
+    return render(request, 'listas1.html')
+
+def poke_post(request):
+    return render(request, 'listas1.html')
+
+def poke_put(request):
+    return render(request, 'listas1.html')
+
+def poke_delete(request):
+    return render(request, 'listas1.html')
